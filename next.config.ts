9ceab2f6781d9c61e588c0bc46c450234a3ab5ee
Cierra2328 <ap28sooners@gmail.com>
2024-11-14 11:25:47 -0600

    deploy

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,23 @@
-import type { NextConfig } from "next";
+import { NextConfig } from 'next';
 
-const nextConfig: NextConfig = {
-  /* config options here */
+const isGithubActions = process.env.GITHUB_ACTIONS || false;
+
+let assetPrefix = '';
+let basePath = '';
+
+if (isGithubActions) {
+  const repo = process.env.GITHUB_REPOSITORY?.replace(/.*?\//, '') || '';
+  assetPrefix = `/${repo}/`;
+  basePath = `/${repo}`;
+}
+
+const config: NextConfig = {
+  output: 'export',
+  assetPrefix,
+  basePath,
+  images: {
+    unoptimized: true,
+  },
 };
 
-export default nextConfig;
+export default config;
