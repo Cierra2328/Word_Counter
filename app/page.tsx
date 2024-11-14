0bc93ab74d48fd93feb5462cb80c1212e7ec3af9
Cierra2328 <ap28sooners@gmail.com>
2024-11-14 11:19:02 -0600

    added app

--- app/page.tsx
+++ app/page.tsx
@@ -1,95 +1,38 @@
-import Image from "next/image";
-import styles from "./page.module.css";
+"use client"
+import { useState } from "react";
 
 export default function Home() {
-  return (
-    <div className={styles.page}>
-      <main className={styles.main}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol>
-          <li>
-            Get started by editing <code>app/page.tsx</code>.
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
 
-        <div className={styles.ctas}>
-          <a
-            className={styles.primary}
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className={styles.logo}
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-            className={styles.secondary}
-          >
-            Read our docs
-          </a>
-        </div>
-      </main>
-      <footer className={styles.footer}>
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+
+  const [wordCount, setWordCount] = useState(0);
+  const [text, setText] = useState("");
+
+  function getCount(event: React.ChangeEvent<HTMLTextAreaElement>) {
+    event.preventDefault();
+    const input = document.getElementById("input") as HTMLTextAreaElement;
+    var value = event.target.value;
+    setText(value);
+    const wordList = value.split(/\s+/).filter(Boolean);
+    var words = wordList.length;
+    setWordCount(words);
+  }
+
+  function erase(event: React.MouseEvent<HTMLButtonElement>){
+    setText("");
+    setWordCount(0);
+    
+  }
+
+  return (
+    <div className="centered">
+      <div className="counter">
+      <h3>Word Counter</h3>
+      <p>Enter some text into the box and get a word count</p>
+      <textarea className="textbox" id="input" value={text} onChange={getCount}></textarea>
+      <br></br>      
+      <button onClick={erase}>Clear Text</button>
+      <p>Your word count is: {wordCount}</p>
+      </div>
     </div>
   );
 }
